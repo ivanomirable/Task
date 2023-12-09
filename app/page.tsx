54e98c912f9fb3aefcbb271b5da59e5c2dd8b7b4
ivanmirable <ivankrivonogov1823@mail.ru>
2023-12-09 23:09:42 +0400

    fixed_position

--- app/page.tsx
+++ app/page.tsx
@@ -12,38 +12,45 @@ import { useActiveContext } from '@/app/context'
 import { useToggle } from './hooks/Toggle'
 
 const OperProd = styled.div`
-  display: flex;
-  position: absolute;
+  width: 100%;
+  height: 100%;
+  position: fixed;
   top: 0;
-  right: 0;
-  bottom: 0;
   left: 0;
-  margin: auto;
-  width:100%;
-  padding 0 20px;
-  margin: 300px auto;
+  display: flex;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
+  overflow: auto;
   flex-direction: row;
   justify-content: center;
   column-gap: 30px;
-  row-gap:30px;
+  row-gap: 30px;
   flex-wrap: wrap;
   @media (max-width: 416px) {
-    column-gap:10px;
+    column-gap: 10px;
   }
   @media (max-width: 373px) {
-    column-gap:2px;
+    column-gap: 2px;
+  }
+`
+const Container = styled.div`
+  justify-content: center;
+  align-items: center;
+  @media (max-width: 373px) {
+    margin-left: 10px;
   }
 `
 
 export default function Home() {
   return (
-    <div>
+    <Container>
       <OperProd>
         {operators.map((operator) => (
           <Operator operator={operator} key={operator.id} />
         ))}
       </OperProd>
       <PayForm />
-    </div>
+    </Container>
   )
 }
